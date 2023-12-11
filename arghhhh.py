73c61b5ba92835427c64574c0f0ad7e4a13e9b9e
mmiwsapp <149761360+mmiwsapp@users.noreply.github.com>
2023-12-11 09:09:12 +0700

    Update arghhhh.py

--- arghhhh.py
+++ arghhhh.py
@@ -4,8 +4,11 @@ import json
 import pandas as pd
 
 
-OPENAI_API_KEY = "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"
-client = openai.OpenAI(api_key=OPENAI_API_KEY)
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password") 
+ 
+#OPENAI_API_KEY = "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"  
+#client = openai.OpenAI(api_key=OPENAI_API_KEY)
+client = openai.OpenAI(api_key=user_api_key) 
 prompt =  """Act as a social media coordinator. Write a product slogan. You will receive product description and
             you should give 20 ideas of product slogan and also give description to support the slogan.
             List the slogan in a Json array, one slogan per line. 
@@ -23,12 +26,8 @@ st.markdown('Input your product description to get slogan ideas. \n\
             The AI will give you 20 ideas of slogan and its description')
 
 #Create and name sidebar
-st.sidebar.header('Choose your product slogan')
-st.sidebar.write("""#### Your OpenAi API key here""")
-#Add text input to the sidebar
-api_key_input = st.sidebar.text_input('Enter your API key:', 'Your OpenAI API key')
- 
-
+#st.sidebar.header('Choose your product slogan')
+#st.sidebar.write("""#### Your OpenAi API key here""")
 
 user_input = st.text_area("Enter product description:", "Your text here")
 
@@ -46,10 +45,11 @@ if st.button('Submit'):
      
     # Show the response from the AI in a box
     st.markdown('**AI response:**')
-    translation_dict = response.choices[0].message.content
+    slogan_dict = response.choices[0].message.content
     
-    sd = json.loads(translation_dict)
+    sd = json.loads(slogan_dict)
     print(sd) 
     suggestion_df = pd.DataFrame.from_dict(sd)
     print(suggestion_df)
     st.table(suggestion_df) 
+    
